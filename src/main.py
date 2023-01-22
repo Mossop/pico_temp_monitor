5a691f52a17ce1e6a185e6de27e33b3e332350f7
Dave Townsend <dtownsend@oxymoronical.com>
2023-01-22 12:51:43 +0000

    Switch to a class for the main operations and track failure counts

--- src/main.py
+++ src/main.py
@@ -9,108 +9,114 @@ from .measurements import Measurement, TAGS, upload, build_tags
 from .net import connect, update_time
 from .log import Logger
 
-MEASUREMENT_INTERVAL = 60
-
-BASE_TICKS = time.monotonic()
-
-SUBMIT_MINIMUM = 1
-
 log = Logger("MAIN")
+
 
 def sleep_s(timeout):
     time.sleep(timeout)
 
 
-def measurement(measurements):
-    with log.safe("Failed to take measurements"):
-        with I2C(board.GP9, board.GP8) as i2c:
-            ht = HTU31D(i2c)
-            ht.humidity_resolution = "0.014%"
-            ht.temp_resolution = "0.012"
-            temp, humidity = ht.measurements
+class Main:
+    MEASUREMENT_INTERVAL = 60
+
+    BASE_TICKS = time.monotonic()
+
+    def __init__(self):
+        self.measurements = []
+        self.failures = 0
+        i2c = I2C(board.GP9, board.GP8)
+        self.htu31d = HTU31D(i2c)
+        self.htu31d.humidity_resolution = "0.014%"
+        self.htu31d.temp_resolution = "0.012"
+
+    def measurement(self):
+        with log.safe("Failed to take measurements"):
+            temp, humidity = self.htu31d.measurements
             reading = Measurement("htu31d")
-            reading.value("temp", temp)
+            reading.tag("serial", str(self.htu31d.serial_number))
+            reading.value("temperature", temp)
             reading.value("humidity", humidity)
 
-            measurements.append(reading)
+            self.measurements.append(reading)
+
+    def monitor(self):
+        with log.safe("Failed to monitor"):
+            reading = Measurement("pico_state")
+            reading.value("ticks", int(time.monotonic() - self.BASE_TICKS))
+            reading.value("freq", microcontroller.cpu.frequency)
+            reading.value("cpu_temp", microcontroller.cpu.temperature)
+            reading.value("voltage", microcontroller.cpu.voltage)
+            reading.value("usb", 1 if runtime.usb_connected else 0)
+            reading.value("serial", 1 if runtime.serial_connected else 0)
+            reading.value("pending", len(self.measurements))
+            reading.value("failures", self.failures)
+            # reading.value("reset_cause", microcontroller.cpu.reset_reason)
+
+            self.measurements.append(reading)
+
+    def submit(self):
+        log.trace("Sending %d measurements" % (len(self.measurements)))
+        with connect() as network:
+            try:
+                update_time(network)
+            except Exception:
+                pass
+
+            upload(self.measurements, network)
+
+    def main_loop(self):
+        log.info("Starting main loop")
+
+        while True:
+            last_measures = time.monotonic()
+            self.monitor()
+            self.measurement()
+
+            with log.safe("Failed to report measurements"):
+                self.failures = self.failures + 1
+                self.submit()
+                self.measurements = []
+                self.failures = 0
+
+            delay = (last_measures + self.MEASUREMENT_INTERVAL) - time.monotonic()
+            if delay > 0:
+                sleep_s(delay)
+
+    def set_time(self):
+        attempts = 1
+        while attempts > 0:
+            with log.safe("Failed to connect"):
+                with connect(timeout = 60) as network:
+                    with log.safe("Failed to update time"):
+                        update_time(network, 3)
+                        return
+
+            sleep_s(2)
+            attempts -= 1
+
+        log.error("Failed to complete initial connection")
+        raise Exception("Failed to complete initial connection")
 
 
-def monitor(measurements):
-    with log.safe("Failed to monitor"):
-        reading = Measurement("pico_state")
-        reading.value("ticks", int(time.monotonic() - BASE_TICKS))
-        reading.value("freq", microcontroller.cpu.frequency)
-        reading.value("cpu_temp", microcontroller.cpu.temperature)
-        reading.value("voltage", microcontroller.cpu.voltage)
-        reading.value("usb", 1 if runtime.usb_connected else 0)
-        reading.value("serial", 1 if runtime.serial_connected else 0)
-        # reading.value("reset_cause", microcontroller.cpu.reset_reason)
+    def run(self):
+        print("Startup. Measurement timeout %d" % (self.MEASUREMENT_INTERVAL))
 
-        measurements.append(reading)
-
-
-def submit(measurements):
-    log.trace("Sending %d measurements" % (len(measurements)))
-    with connect() as network:
+        # Make sure we get an accurate time before running the loop
         try:
-            update_time(network)
+            self.set_time()
         except Exception:
-            pass
+            # Failed to get the time, try rebooting in case there is a hardware issue
+            # machine.reset()
+            return
 
-        upload(measurements, network)
+        build_tags()
+        print("Tags:")
+        for k in TAGS.keys():
+            print("  %s: %s" % (k, TAGS[k]))
+        print()
 
-
-def main_loop():
-    measurements = []
-
-    log.info("Starting main loop")
-
-    while True:
-        last_measures = time.monotonic()
-        measurement(measurements)
-        monitor(measurements)
-
-        with log.safe("Failed to report measurements"):
-            if len(measurements) >= SUBMIT_MINIMUM:
-                submit(measurements)
-                measurements = []
-
-        delay = (last_measures + MEASUREMENT_INTERVAL) - time.monotonic()
-        if delay > 0:
-            sleep_s(delay)
-
-
-def set_time():
-    attempts = 1
-    while attempts > 0:
-        with log.safe("Failed to connect"):
-            with connect(timeout = 60) as network:
-                with log.safe("Failed to update time"):
-                    update_time(network, 3)
-                    return
-
-        sleep_s(2)
-        attempts -= 1
-
-    log.error("Failed to complete initial connection")
-    raise Exception("Failed to complete initial connection")
-
+        self.main_loop()
 
 def main():
-    print("Startup. Measurement timeout %d" % (MEASUREMENT_INTERVAL))
-
-    # Make sure we get an accurate time before running the loop
-    try:
-        set_time()
-    except Exception:
-        # Failed to get the time, try rebooting in case there is a hardware issue
-        # machine.reset()
-        return
-
-    build_tags()
-    print("Tags:")
-    for k in TAGS.keys():
-        print("  %s: %s" % (k, TAGS[k]))
-    print()
-
-    main_loop()
+    main = Main()
+    main.run()
